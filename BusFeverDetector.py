3d695c3dba11a3a37b2046789727ea671521b90a
Frédéric Desbiens <frederic.desbiens@eclipse-foundation.org>
2021-04-28 15:41:19 -0400

    Add copyrights and license identifier

--- BusFeverDetector.py
+++ BusFeverDetector.py
@@ -1,3 +1,13 @@
+# Copyright (c) 2021 Eclipse Foundation.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Original Author: Joongho Kim
+
 """This example is for Raspberry Pi (Linux) only!
    It will not work on microcontrollers running CircuitPython!"""
  
